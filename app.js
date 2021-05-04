49f6b948c0ede396757b3c61c675628d4676550b
Veronika Stuchlíková <stuchlikova.veronika@post.cz>
2021-05-04 19:02:31 +0200

    javascript

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+const closedFace = document.querySelector('.closed');
+const openedFace = document.querySelector('.opened');
 
- 
+
+closedFace.addEventListener('click', () => {
+    if(openedFace.classList.contains('opened')) {
+       openedFace.classList.add('active');
+       closedFace.classList.remove('active');
+    }
+});
+
+openedFace.addEventListener('click', open);
+
+function open() {
+    if(closedFace.classList.contains('closed')) {
+        closedFace.classList.add('active');
+        openedFace.classList.remove('active');
+    }
+}
